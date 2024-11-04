3e088f04471a499395686e014215242bda8a820a
julia <juliafu713@gmail.com>
2024-11-04 17:22:07 -0300

    feito

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@ import "@passageidentity/passage-elements/passage-auth";
 <template>
   <h1>Login</h1>
   <div class="authContainer">
-    <passage-auth app-id="GKj1efF61W4xT1JFyDuKPfsK"></passage-auth>
+    <passage-auth app-id="V0alwA7XSzKK5hRenPSvbiYf"></passage-auth>
   </div>
 </template>
 
